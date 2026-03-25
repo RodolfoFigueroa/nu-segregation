5e591ff3cc43d74679c793862e25bf1c4e88f0e2
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2026-03-24 20:23:03 -0600

    FInish survey asset

--- src/nu_segregation/definitions.py
+++ src/nu_segregation/definitions.py
@@ -1,6 +1,8 @@
-import dagster as dg
 from pathlib import Path
+
 from dagster_components.resources import PostGISResource
+
+import dagster as dg
 from nu_segregation.defs.resources import PathResource
 
 
@@ -13,10 +15,10 @@ def definitions() -> dg.Definitions:
             "postgis_resource": PostGISResource(
                 host=dg.EnvVar("POSTGRES_HOST"),
                 port=dg.EnvVar("POSTGRES_PORT"),
-                username=dg.EnvVar("POSTGRES_USER"),
+                user=dg.EnvVar("POSTGRES_USER"),
                 password=dg.EnvVar("POSTGRES_PASSWORD"),
                 db=dg.EnvVar("POSTGRES_DB"),
-            )
+            ),
         }
     )
     return dg.Definitions.merge(defs, extra_defs)
--- src/nu_segregation/defs/assets/survey.py
+++ src/nu_segregation/defs/assets/survey.py
@@ -1,158 +1,208 @@
-import dagster as dg
-from dagster_components.resources import PostGISResource
-import os
-from pathlib import Path
-import pandas as pd
-from nu_segregation.defs.resources import PathResource
-import zipfile
 import tempfile
+import zipfile
+from pathlib import Path
+
+import pandas as pd
+from dagster_components.partitions import zone_partitions
+from dagster_components.resources import PostGISResource
+from dagster_components.utils import cast_all_columns_to_numeric
+
+import dagster as dg
+from nu_segregation.defs.resources import PathResource
 
 
-def extract_and_read_op_factory(top_dir_path: str, fname: str) -> dg.OpDefinition:
-    @dg.op
-    def _op(path_resource: PathResource) -> pd.DataFrame:
-        with z
-
-    return _op
+def fix_folioviv(s: pd.Series) -> pd.Series:
+    return s.astype(str).str.zfill(10)
 
 
-@dg.asset
-def survey(
-    path_resource: PathResource, met_zone_codes: list, linking_cols: list[str], q: int
-) -> pd.DataFrame:
-    data_path = Path(data_path)
-
-    # Folio de vivienda
-    path_folio = data_path / "survey/enigh2018_ns_viviendas_csv.zip"
-    # Ingreso
-    path_ing = data_path / "survey/enigh2018_ns_ingresos_csv.zip"
-    # conjunto de hogares para obtener la info de conexión a internet
-    path_hog = data_path / "survey/enigh2018_ns_hogares_csv.zip"
-    # Información demogŕafica
-    path_pob = data_path / "survey/enigh2018_ns_poblacion_csv.zip"
-
-    df_folio = pd.read_csv(path_folio, usecols=["folioviv", "ubica_geo"])
-
-    df_ing = pd.read_csv(
-        path_ing,
-        usecols=["folioviv", "foliohog", "numren", "ing_tri"],
-        dtype={
-            "folioviv": int,
-            "foliohog": int,
-            "numren": int,
-            "ing_tri": pd.Float64Dtype(),
-        },
-    )
-
-    df_hog = pd.read_csv(
-        path_hog,
-        usecols=["folioviv", "foliohog", "conex_inte"],
-        dtype={"folioviv": int, "foliohog": int, "conex_inte": pd.Int64Dtype()},
-    )
-
-    df_poblacion = pd.read_csv(
-        path_pob,
-        usecols=[
-            "folioviv",
-            "foliohog",
-            "numren",
-            "sexo",
-            "edad",
-            "edo_conyug",
-            "nivelaprob",
-            "inst_1",
-            "inst_6",
-        ],
-        na_values=[" "],
-        dtype={
-            "folioviv": int,
-            "foliohog": int,
-            "numren": int,
-            "sexo": pd.Int64Dtype(),
-            "edad": pd.Int64Dtype(),
-            "edo_conyug": pd.Int64Dtype(),
-            "nivelaprob": pd.Int64Dtype(),
-            "inst_1": pd.Int64Dtype(),
-            "inst_6": pd.Int64Dtype(),
-        },
-    ).fillna(0)
-
-    # Agregate income for duplicate individuals
-    ing_agg = df_ing.groupby(["folioviv", "foliohog", "numren"]).agg(sum).reset_index()
-
-    # Filter for state and metropolitan zone
-    df_location = df_folio[df_folio.ubica_geo.isin(met_zone_codes)]
-
-    # Filter for working population and add location data
-    df_poblacion = df_poblacion[df_poblacion["edad"] >= 15]
-
-    # Merge all dataframes
-    df_ind_orig = (
-        df_location.merge(df_poblacion, how="left")
-        .merge(ing_agg, how="left")
-        .merge(df_hog, how="left")
-        .reset_index(drop=True)
-    )
-
-    # Rename columns to match link and targer variable names
-    df_ind_orig = df_ind_orig.rename(
-        columns={
-            "des_mun": "Municipio",
-            "sexo": "Sexo",
-            "edad": "Edad",
-            "nivelaprob": "Nivel",
-            "edo_conyug": "EstadoConyu",
-            "inst_1": "SeguroIMSS",
-            "inst_6": "SeguroPriv",
-            "conex_inte": "ConexionInt",
-            "ing_tri": "Ingreso",
-        }
-    )
-
-    # Keep a df with linking variables, make variables explicitly categorical.
-    df_ind = df_ind_orig[[*linking_cols, "Ingreso"]].copy()
-
-    df_ind["Sexo"] = df_ind["Sexo"].astype("category")
-    df_ind["Sexo"] = df_ind["Sexo"].cat.rename_categories({1: "m", 2: "f"})
-
-    df_ind["Edad"] = pd.cut(
-        df_ind["Edad"],
-        bins=[15, 64, 200],
-        labels=["p15_64", "p65mas"],
-        include_lowest=True,
-    )
-
-    df_ind["Nivel"] = pd.cut(
-        df_ind["Nivel"],
-        bins=[-1, 0, 2, 3, 100],
-        labels=["ninguno", "primaria", "secundaria", "posbasica"],
-    )
-
-    df_ind["SeguroIMSS"] = df_ind["SeguroIMSS"].astype("category")
-    df_ind["SeguroIMSS"] = df_ind["SeguroIMSS"].cat.rename_categories(
-        {0: "no_imss", 1: "imss"},
-    )
-
-    df_ind["SeguroPriv"] = df_ind["SeguroPriv"].astype("category")
-    df_ind["SeguroPriv"] = df_ind["SeguroPriv"].cat.rename_categories(
-        {0: "no_privado", 6: "privado"},
-    )
-
-    df_ind["ConexionInt"] = df_ind["ConexionInt"].astype("category")
-    df_ind["ConexionInt"] = df_ind["ConexionInt"].cat.rename_categories(
-        {1: "internet", 2: "no_internet"},
-    )
-
-    if "EstadoConyu" in linking_cols:
-        df_ind["EstadoConyu"] = pd.cut(
-            df_ind["EstadoConyu"],
-            bins=[-1, 2, 5, 100],
-            labels=["casada", "separada", "soltera"],
+@dg.op
+def get_muns_from_zone(
+    context: dg.OpExecutionContext, postgis_resource: PostGISResource
+) -> list[str]:
+    with postgis_resource.connect() as conn:
+        df = pd.read_sql(
+            """
+            SELECT "CVEGEO" FROM census_2020_mun
+            WHERE "CVE_MET" = %(zone)s
+            """,
+            conn,
+            params={"zone": context.partition_key},
         )
+    return df["CVEGEO"].tolist()
 
-    # Find the bin ranges
-    df_ind["Ingreso"] = pd.qcut(df_ind["Ingreso"], q, labels=list(range(1, q + 1)))
 
-    df_ind["Ingreso_orig"] = df_ind_orig.Ingreso
+stems = ["vivienda", "ingresos", "hogares", "poblacion"]
 
-    return df_ind
+
+@dg.op(out={stem: dg.Out() for stem in stems})
+def extract_enigh_census(
+    path_resource: PathResource,
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    data_path = Path(path_resource.data_path)
+
+    out = []
+    with (
+        tempfile.TemporaryDirectory() as tmpdir,
+        zipfile.ZipFile(data_path / "conjunto_de_datos_enigh_2018_ns_csv.zip") as zf,
+    ):
+        zf.extractall(tmpdir)
+        tmpdir_path = Path(tmpdir)
+
+        for stem in stems:
+            subdir_name = f"conjunto_de_datos_{stem}_enigh_2018_ns"
+            fpath = next(
+                (tmpdir_path / subdir_name / "conjunto_de_datos").glob(
+                    "conjunto_de_datos_*.csv"
+                )
+            )
+            out.append(pd.read_csv(fpath))
+
+    return tuple(out)
+
+
+@dg.op
+def process_fol_df(df_fol: pd.DataFrame, muns: list[str]) -> pd.DataFrame:  # noqa: ARG001
+    return (
+        df_fol[["folioviv", "ubica_geo"]]
+        .assign(
+            folioviv=lambda df: df["folioviv"].transform(fix_folioviv),
+            ubica_geo=lambda df: df["ubica_geo"].astype(str).str.zfill(5),
+        )
+        .query("ubica_geo in @muns")
+    )
+
+
+@dg.op
+def process_income_df(df_income: pd.DataFrame) -> pd.DataFrame:
+    return (
+        df_income[["folioviv", "foliohog", "numren", "ing_tri"]]
+        .assign(folioviv=lambda df: df["folioviv"].transform(fix_folioviv))
+        .pipe(cast_all_columns_to_numeric, ignore=["folioviv"], errors="raise")
+        .groupby(["folioviv", "foliohog", "numren"])
+        .agg(sum)
+        .reset_index()
+    )
+
+
+@dg.op
+def process_home_df(df_home: pd.DataFrame) -> pd.DataFrame:
+    return (
+        df_home[["folioviv", "foliohog", "conex_inte"]]
+        .assign(folioviv=lambda df: df["folioviv"].transform(fix_folioviv))
+        .pipe(cast_all_columns_to_numeric, ignore=["folioviv"], errors="raise")
+    )
+
+
+@dg.op
+def process_pop_df(df_pop: pd.DataFrame) -> pd.DataFrame:
+    return (
+        df_pop[
+            [
+                "folioviv",
+                "foliohog",
+                "numren",
+                "sexo",
+                "edad",
+                "edo_conyug",
+                "nivelaprob",
+                "inst_1",
+                "inst_6",
+            ]
+        ]
+        .replace(" ", pd.NA)
+        .fillna(0)
+        .assign(folioviv=lambda df: df["folioviv"].transform(fix_folioviv))
+        .pipe(cast_all_columns_to_numeric, ignore=["folioviv"], errors="raise")
+        .query("edad >= 15")
+    )
+
+
+@dg.op
+def merge_dfs(
+    df_fol: pd.DataFrame,
+    df_income: pd.DataFrame,
+    df_home: pd.DataFrame,
+    df_pop: pd.DataFrame,
+) -> pd.DataFrame:
+    return (
+        df_fol.merge(df_pop, how="left")
+        .merge(df_income, how="left")
+        .merge(df_home, how="left")
+        .reset_index(drop=True)
+        .rename(
+            columns={
+                "des_mun": "Municipio",
+                "sexo": "Sexo",
+                "edad": "Edad",
+                "nivelaprob": "Nivel",
+                "edo_conyug": "EstadoConyu",
+                "inst_1": "SeguroIMSS",
+                "inst_6": "SeguroPriv",
+                "conex_inte": "ConexionInt",
+                "ing_tri": "Ingreso",
+            }
+        )
+        .filter(
+            [
+                "Sexo",
+                "Edad",
+                "Nivel",
+                "SeguroIMSS",
+                "SeguroPriv",
+                "ConexionInt",
+                "Ingreso",
+            ],
+            axis="columns",
+        )
+        .assign(
+            Sexo=lambda df: (
+                df["Sexo"].astype("category").cat.rename_categories({1: "m", 2: "f"})
+            ),
+            Edad=lambda df: (
+                pd.cut(
+                    df["Edad"],
+                    bins=[15, 64, 200],
+                    labels=["p15_64", "p65mas"],
+                    include_lowest=True,
+                ),
+            ),
+            Nivel=lambda df: (
+                pd.cut(
+                    df["Nivel"],
+                    bins=[-1, 0, 2, 3, 100],
+                    labels=["ninguno", "primaria", "secundaria", "posbasica"],
+                ),
+            ),
+            SeguroIMSS=lambda df: (
+                df["SeguroIMSS"]
+                .astype("category")
+                .cat.rename_categories({0: "no_imss", 1: "imss"})
+            ),
+            SeguroPriv=lambda df: (
+                df["SeguroPriv"]
+                .astype("category")
+                .cat.rename_categories({0: "no_privado", 6: "privado"})
+            ),
+            ConexionInt=lambda df: (
+                df["ConexionInt"]
+                .astype("category")
+                .cat.rename_categories({1: "internet", 2: "no_internet"})
+            ),
+            Ingreso_new=lambda df: pd.qcut(df["Ingreso"], 5, labels=list(range(1, 6))),
+        )
+        .rename(columns={"Ingreso": "Ingreso_orig"})
+        .rename(columns={"Ingreso_new": "Ingreso"})
+    )
+
+
+@dg.graph_asset(partitions_def=zone_partitions, group_name="survey")
+def survey():
+    muns = get_muns_from_zone()
+
+    enigh_census_map = extract_enigh_census()
+
+    df_income = process_income_df(enigh_census_map[1])
+    df_folio = process_fol_df(enigh_census_map[0], muns)
+    df_home = process_home_df(enigh_census_map[2])
+    df_pop = process_pop_df(enigh_census_map[3])
+    return merge_dfs(df_folio, df_income, df_home, df_pop)
